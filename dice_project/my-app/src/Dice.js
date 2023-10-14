7af4212ad0f0e585b67c699c018cc02d7233cd49
Fatih Özcan <58815565+fatih3457@users.noreply.github.com>
2023-10-14 10:29:31 +0300

    first commit

---
+++ dice_project/my-app/src/Dice.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import "./Dice.css"
+
+class Dice extends Component {
+    render() {
+        let cls = `fa-solid fa-8x fa-dice-${this.props.face} Dice`
+        return <i className={cls}></i> 
+    }
+}
+
+export default Dice
