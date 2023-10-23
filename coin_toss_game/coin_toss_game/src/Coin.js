403239a0e414f90c8f14ad664721528968786fe8
Fatih Özcan <58815565+fatih3457@users.noreply.github.com>
2023-10-24 00:07:09 +0300

    update Coin.css

--- coin_toss_game/coin_toss_game/src/Coin.js
+++ coin_toss_game/coin_toss_game/src/Coin.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
+import './Coin.css'
 
 class Coin extends Component {
     render() {
         return (
-            <div>
+            <div className='Coin'>
                 <img src= {this.props.info.imgSrc} alt={this.props.info.side}/>
             </div>
         )
